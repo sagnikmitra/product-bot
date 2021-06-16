d3110ddcba0201b0b1c2300cb76a9892e33905c0
sagnikmitra <sagnikmitra123@gmail.com>
2021-06-16 19:14:14 +0530

    id price block lint: commented streamlit

--- amazonPrice.py
+++ amazonPrice.py
@@ -3,6 +3,7 @@ import bs4
 import pandas as pd
 from PIL import Image
 import streamlit as st
+
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
@@ -15,10 +16,12 @@ soup = bs4.BeautifulSoup(res.content, features='lxml')
 
 amount = (soup.find(id='priceblock_ourprice').get_text().replace(
     "₹", "").strip())
+
+amount = (soup.find(id='priceblock_ourprice').get_text().replace(
+    "₹", "").strip())
+
 print(float(amount.replace(",", "")))
-st.write(float(amount.replace(",", "")))
+# st.write(float(amount.replace(",", "")))
 name = (soup.find(id='productTitle').get_text().strip())
 print(str(name))
-st.write(str(name))
-image = soup.find(id='landingImage')
-st.image(image)
+# st.write(str(name))
